d811efa1849882cee377d1a33864150df216c504
jasmijn301 <454893@student.fontys.nl>
2024-01-12 17:07:57 +0100

    eventemitter toegevoegd voor de button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input  } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,10 +8,11 @@ import { Component, Input  } from '@angular/core';
 export class ButtonComponent {
   @Input() text: string = '';
   @Input() color: string = '';
+  @Output() btnClick = new EventEmitter()
 
   onClick() {
-    console.log("add");
+    this.btnClick.emit();
   }
-  // Je kunt hier de informatie wat er moet gebeuren inzetten, maar is beter in een ander bestand zodat je deze button vaker kunt gebruiken.
+  // Je kunt hier de informatie wat er moet gebeuren inzetten, maar is beter in een ander bestand zodat je deze button vaker kunt gebruiken. Daarom gebruiken we hier een eventemitter
 
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   title: string = 'Task tracker';
 
+  toggleAddTask() {
+    console.log("object")
+  }
 }
