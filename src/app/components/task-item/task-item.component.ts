285a6a756a00a07ea1f1e35002068e555b10b108
jasmijn301 <454893@student.fontys.nl>
2024-01-15 14:09:09 +0100

    css task-item gecopied uit github vd tutorial, fortawesome toegevoegd en toegepast > nu automatisch kruisjes achter de tasks

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
 // Deze moeten we erbij zetten omdat Task onze interface is
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -13,4 +14,7 @@ export class TaskItemComponent {
 
   // @Input() task!: Task;
   // Dit kan ook: verteld typescript dat hij moet ignoren dat het not initialized is in de contructor.
+
+  faTimes = faTimes;
+  // Zodat je het nu in je html kan gebruiken.
 }
