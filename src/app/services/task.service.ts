af0614b9f92a25fb9f5e92b0074b671e4d033a85
jasmijn301 <454893@student.fontys.nl>
2024-01-15 15:09:04 +0100

    je ziet hetzelfde op je browser, maar nu komt de data via een http request.

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable,of } from 'rxjs'
 import {Task} from '../Task';
 import {TASKS} from '../mock-tasks';
@@ -7,12 +8,12 @@ import {TASKS} from '../mock-tasks';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
   // We doen het op dezelfde manier als we deden in de component, alleen nu we want to call this getTasks from our component, instead of bringin it directly in.
 }
