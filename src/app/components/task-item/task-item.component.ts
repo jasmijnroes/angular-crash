67c3d48f73fc175b1256a68cb8c4c8f28800eaeb
jasmijn301 <454893@student.fontys.nl>
2024-01-15 13:54:15 +0100

    Code is nu efficienter met de enabled strictPropertyInitialization rule in typescript.

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Task } from '../../Task';
 // Deze moeten we erbij zetten omdat Task onze interface is
 
@@ -7,17 +7,10 @@ import { Task } from '../../Task';
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
-export class TaskItemComponent implements OnInit {
+export class TaskItemComponent {
   @Input() task: Task = {"text": "test", "day": "test", "reminder": true, "id": 1};
   // maar dit werkt niet als er no sensible default value is voor task.
 
   // @Input() task!: Task;
   // Dit kan ook: verteld typescript dat hij moet ignoren dat het not initialized is in de contructor.
-
-  constructor() { }
-
-  ngOnInit() {
-    // You can safely access this.task here
-    console.log(this.task);
-  }
 }
