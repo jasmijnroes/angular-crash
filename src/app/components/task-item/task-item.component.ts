213d37ba4e382c8cbdd13109e88b21dceb3a81ff
jasmijn301 <454893@student.fontys.nl>
2024-01-15 13:52:24 +0100

    de tasks werken nu > error kwam doordat mijn strictPropertyInitialization is enabled.

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,13 +1,23 @@
-import { Component, Input } from '@angular/core';
-import {Task} from '../../Task'
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from '../../Task';
 // Deze moeten we erbij zetten omdat Task onze interface is
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.css'
+  styleUrls: ['./task-item.component.css']
 })
-export class TaskItemComponent {
-  @Input() task?: Task;
-  // In de tutorial geen vraagteken, en wel =string erachter, maar dan werkte het bij mij niet.
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = {"text": "test", "day": "test", "reminder": true, "id": 1};
+  // maar dit werkt niet als er no sensible default value is voor task.
+
+  // @Input() task!: Task;
+  // Dit kan ook: verteld typescript dat hij moet ignoren dat het not initialized is in de contructor.
+
+  constructor() { }
+
+  ngOnInit() {
+    // You can safely access this.task here
+    console.log(this.task);
+  }
 }
