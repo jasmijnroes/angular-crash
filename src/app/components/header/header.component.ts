c0a500844b3e8ec0bd77846cba2bb609acfec188
jasmijn301 <454893@student.fontys.nl>
2024-01-16 21:40:16 +0100

    ui service aangemaakt > begin gemaakt voor het laten werken van de 'add' button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnDestroy {
   title: string = 'Task tracker';
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => this.showAddTask = value);
+    // In de tutorial is deze constructor leeg, maar omdat ik een nieuwere versie van angualr gebruik, moet ik deze code toevoegen.
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+  // Ook ngondestroy is niet vanuit de tutorial.
+
 
   toggleAddTask() {
-    console.log("object")
+    this.uiService.toggleAddTask();
   }
 }
